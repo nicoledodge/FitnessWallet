47ec762b0f9f9b266e0261877bc24e6b1ca5686a
nicoledodge <nicoledodge5@gmail.com>
2021-11-17 18:24:45 -0600

    finished indexedb

--- public/db.js
+++ public/db.js
@@ -2,3 +2,65 @@
 //check if the app is online
 //delete records when done
 //addeventlistener for app coming back online
+
+let db;
+let budgetVersion;
+
+const request = indexedDB.open('BudgetDB', budgetVersion || 21);
+
+request.onupgradeneeded = function (evt) {
+    const { oldVersion } = evt;
+    const newVersion = evt.newVersion || db.version;
+
+    db = evt.target.result;
+
+    if (db.objectStoreNames.length === 0) {
+        db.createObjectStore('BudgetStore', { autoIncrement: true });
+    }
+};
+
+request.onerror = function (evt) {
+    console.log(`Request error: ${evt.target.errorCode}`);
+};
+
+function checkDatabase() {
+    let transaction = db.transaction(['BudgetStore'], 'readwrite');
+    const store = transaction.objectStore('BudgetStore');
+    const getAll = store.getAll();
+
+    getAll.onsuccess = function () {
+        if (getAll.result.length > 0) {
+            fetch('/api/transaction/bulk', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json',
+                },
+            })
+                .then((response) => response.json())
+                .then((res) => {
+                    if (res.length !== 0) {
+                        transaction = db.transaction(['BudgetStore'], 'readwrite');
+                        const currentStore = transaction.objectStore('BudgetStore');
+                        currentStore.clear();
+                    }
+                });
+        }
+    };
+}
+
+request.onsuccess = function (evt) {
+    db = evt.target.result;
+    if (navigator.onLine) {
+        checkDatabase();
+    }
+};
+
+const saveRecord = (record) => {
+    const transaction = db.transaction(['BudgetStore'], 'readwrite');
+    const store = transaction.objectStore('BudgetStore');
+    store.add(record);
+};
+
+window.addEventListener('online', checkDatabase);
--- public/service-worker.js
+++ public/service-worker.js
@@ -10,22 +10,16 @@ const FILES_TO_CACHE = [
     "/icons/icon-512x512.png"
 ]
 
-// activate
-self.addEventListener("activate", function(evt) {
+// install
+self.addEventListener("install", function(evt) {
     evt.waitUntil(
-        caches.keys().then(keyList => {
-            return Promise.all(
-                keyList.map(key => {
-                    if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-                        console.log("Removing old cache data", key);
-                        return caches.delete(key);
-                    }
-                })
-            );
+        caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/transaction"))
+    evt.waitUntil(
+        caches.open(CACHE_NAME).then(function(cache){
+            return cache.addAll(FILES_TO_CACHE);
         })
     );
-
-    self.clients.claim();
+    self.skipWaiting();
 });
 
 // fetch
@@ -35,7 +29,6 @@ self.addEventListener("fetch", function(evt) {
             caches.open(DATA_CACHE_NAME).then(cache => {
                 return fetch(evt.request)
                     .then(response => {
-                        // If the response was good, clone it and store it in the cache.
                         if (response.status === 200) {
                             cache.put(evt.request.url, response.clone());
                         }
@@ -51,12 +44,12 @@ self.addEventListener("fetch", function(evt) {
 
         return;
     }
-
+//respond with html pages of cached data
+    //fetch
     evt.respondWith(
-        caches.open(CACHE_NAME).then(cache => {
-            return cache.match(evt.request).then(response => {
-                return response || fetch(evt.request);
-            });
+        caches.match(evt.request).then(function(response) {
+            return response || fetch(evt.request);
         })
     );
 });
+
