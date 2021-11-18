599135348bf96bedb4a6691b3e1c054acaa64220
nicoledodge <nicoledodge5@gmail.com>
2021-11-17 18:27:15 -0600

    got serice worker working?

--- public/service-worker.js
+++ public/service-worker.js
@@ -22,6 +22,22 @@ self.addEventListener("install", function(evt) {
     self.skipWaiting();
 });
 
+self.addEventListener("activate", function (evt) {
+    evt.waitUntil(
+        caches.keys().then(keyList => {
+            return Promise.all(
+                keyList.map(key => {
+                    if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
+    );
+    self.clients.claim();
+});
+
+
 // fetch
 self.addEventListener("fetch", function(evt) {
     if (evt.request.url.includes("/api/")) {
